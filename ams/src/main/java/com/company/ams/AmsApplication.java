61834ce81745eb6aa1e9cf7a42ed665a1721493a
mohamed hajjej <hajjej.med@gmail.com>
2023-11-19 00:45:47 +0100

    creation du projet

---
+++ ams/src/main/java/com/company/ams/AmsApplication.java
@@ -0,0 +1,15 @@
+package com.company.ams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AmsApplication.class, args);
+		System.out.println("Hello world!");
+	}
+
+}
